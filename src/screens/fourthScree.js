3cab4fe7cd961c239ad41b7869af83efdb01d87d
surya21112000 <surya@market-intellect.com>
2023-03-11 21:50:17 +0530

    completed all screens for mob

--- src/screens/fourthScree.js
+++ src/screens/fourthScree.js
@@ -1,67 +1,115 @@
 
 import { LogoLinkedin } from 'react-ionicons'
-import React from 'react'
+import React,{useState} from 'react'
 import '../screens/fourthScreen.css'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+const Alert = (props) => {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+};
 
 const FourthScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'message') {
+      setMessage(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission logic here
+    if (isValidEmail(email)) {
+      setEmailError(false);
+      // Perform form submission logic here
+      setShowSnackbar(true);
+      // Reset form fields after submission
+      setName('');
+      setEmail('');
+      setMessage('');
+    } else {
+      setEmailError(true);
+    }
+  };
+
+  const isValidEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  const handleSnackbarClose = () => {
+    setShowSnackbar(false);
+  };
+
   return (
    <body style={{background:"#ffd2d2"}} >
     <div className='main2'>
-    <div class="container">
-    <div class="screen">
-      <div class="screen-header">
-        <div class="screen-header-left">
-          <div class="screen-header-button close"></div>
-          <div class="screen-header-button maximize"></div>
-          <div class="screen-header-button minimize"></div>
+    <div className='container'>
+    <div className="contact-form">
+      <h1 className='contact'>CONTACT ME</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={handleInputChange}
+            required
+          />
         </div>
-        <div class="screen-header-right">
-          <div class="screen-header-ellipsis"></div>
-          <div class="screen-header-ellipsis"></div>
-          <div class="screen-header-ellipsis"></div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={handleInputChange}
+            required
+          />
         </div>
-      </div>
-      <div class="screen-body">
-        <div class="screen-body-item left">
-          <div class="app-title">
-            <span>CONTACT ME</span>
-            
-          </div>
-          <div class="app-contact">FOLLOW ME  <br></br>  <LogoLinkedin height='20px' width='20px'  color={"blue"} style={{cursor:"pointer"}} onClick={()=>{
-            window.open("https://www.linkedin.com/in/surya-rengadurai-07ab8624b")
-          }} /></div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            value={message}
+            onChange={handleInputChange}
+            required
+          />
         </div>
-        <div class="screen-body-item">
-          <div class="app-form">
-            <div class="app-form-group">
-              <input class="app-form-control" id="title" placeholder="NAME" 
-              style={{color:"#a9a7a7"}}/>
-              
-            </div>
-            <div class="app-form-group">
-              <input class="app-form-control" placeholder="EMAIL" />
-            </div>
-            <div class="app-form-group">
-              <input class="app-form-control" placeholder="CONTACT NO" />
-            </div>
-            <div class="app-form-group message">
-              <input class="app-form-control" placeholder="MESSAGE" /> <br></br>
-              
-            </div>
-            
-           
-            
-          </div>
-          
-        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div className="social-links">
         
+        <a href="https://www.linkedin.com/in/surya-rengadurai-07ab8624b" target="_blank">
+          <LogoLinkedin height={5} />
+        </a>
       </div>
+      <Snackbar
+        open={showSnackbar}
+        autoHideDuration={2000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert onClose={handleSnackbarClose} severity="success">
+          Message sent
+        </Alert>
+      </Snackbar>
     </div>
-  
-  </div>
  
     </div>
-
+</div>
    </body>
   )
 }
